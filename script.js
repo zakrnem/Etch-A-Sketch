304f4ffbfdc29f09c08525294e540218224d1cb9
zakrasty <zakrnem@zohomail.com>
2022-09-01 22:42:47 -0600

    Adjustable grid size feature

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ let container = document.querySelector('.container');
 container.style.width = '500px';
 let containerWidth = parseInt(container.style.width);
 let sideQuantity = 16;
-let gridSize = document.addEventListener('change', createGrid);
+let gridSize = document.addEventListener('change', resizeGrid);
 
  function createGrid(sideQuantity) {
-    // console.log(e.target.value)
-    // sideQuantity = e.target.value;
     let gridWidth = (containerWidth/sideQuantity)-4;
     let borderRadius = gridWidth*0.1;
 
@@ -16,7 +14,26 @@ let gridSize = document.addEventListener('change', createGrid);
         grid.style.cssText = `width: ${gridWidth}px; border-radius: ${borderRadius}px`;
         container.appendChild(grid);
     }
-
 }
 
 createGrid(sideQuantity);
+
+
+function resizeGrid(e) {
+    console.log(e.target.value);
+    sideQuantity = e.target.value;
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+    let gridWidth = (containerWidth/sideQuantity)-4;
+    let borderRadius = gridWidth*0.1;
+
+    for (let i = sideQuantity**2; i > 0 ; i--) {
+        let grid = document.createElement('div');
+        grid.className = 'grid-element';
+        grid.style.cssText = `width: ${gridWidth}px; border-radius: ${borderRadius}px`;
+        container.appendChild(grid);
+    }
+}
