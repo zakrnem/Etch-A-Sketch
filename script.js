1f8441a0081145d6ed8eb249bdde63e9e3ca10f0
zakrasty <zakrnem@zohomail.com>
2022-09-02 21:38:17 -0600

    Working game

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let container = document.querySelector('.container');
 container.style.width = '500px';
 let containerWidth = parseInt(container.style.width);
-let sideQuantity = 16;
+let sideQuantity = 24;
 let gridSize = document.addEventListener('change', resizeGrid);
 
  function createGrid(sideQuantity) {
-    let gridWidth = (containerWidth/sideQuantity)-4;
-    let borderRadius = gridWidth*0.1;
+    let gridWidth = (containerWidth/sideQuantity)-2; /* Create a variable for this formula */
+    let borderRadius = gridWidth*0.05;
 
     for (let i = sideQuantity**2; i > 0 ; i--) {
         let grid = document.createElement('div');
@@ -27,8 +27,8 @@ function resizeGrid(e) {
         container.removeChild(container.firstChild);
     }
     
-    let gridWidth = (containerWidth/sideQuantity)-4;
-    let borderRadius = gridWidth*0.1;
+    let gridWidth = (containerWidth/sideQuantity)-2; /* Create a variable for this formula */
+    let borderRadius = gridWidth*0.05;
 
     for (let i = sideQuantity**2; i > 0 ; i--) {
         let grid = document.createElement('div');
@@ -37,3 +37,21 @@ function resizeGrid(e) {
         container.appendChild(grid);
     }
 }
+
+let mouseHover = container.addEventListener('mouseover', paintBox)
+
+function paintBox(e) {
+    console.log(e)
+    if (e.target.className === 'grid-element' && e.ctrlKey === false) {
+        let selectedBox = e.target;
+        selectedBox.style.backgroundColor = 'grey';
+    }
+        //let gridElement = document.getElementsByClassName('grid-element');
+        //console.log(gridElement)
+}
+
+let resetButton = footer.querySelector('.reset-button')
+    resetButton.addEventListener('click', () => {
+        e.preventDefault();
+        /* insert background for each grid box function */
+    })
