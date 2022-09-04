3047f2ca5d9b6e39ae9650eb1833390ade873e98
zakrasty <zakrnem@zohomail.com>
2022-09-04 08:47:34 -0600

    Create function for repetitive processes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let sideQuantity = 24;
 let gridSize = document.addEventListener('change', resizeGrid);
 
  function createGrid(sideQuantity) {
-    let gridWidth = (containerWidth/sideQuantity)-2; /* Create a variable for this formula */
+    let gridWidth = (containerWidth/sideQuantity)-2;
     let borderRadius = gridWidth*0.05;
 
     for (let i = sideQuantity**2; i > 0 ; i--) {
@@ -18,47 +18,32 @@ let gridSize = document.addEventListener('change', resizeGrid);
 
 createGrid(sideQuantity);
 
+function clearGrid () {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
 function resizeGrid(e) {
 
     sideQuantity = e.target.value;
-    
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    
-    let gridWidth = (containerWidth/sideQuantity)-2; /* Create a variable for this formula */
-    let borderRadius = gridWidth*0.05;
-
-    for (let i = sideQuantity**2; i > 0 ; i--) {
-        let grid = document.createElement('div');
-        grid.className = 'grid-element';
-        grid.style.cssText = `width: ${gridWidth}px; border-radius: ${borderRadius}px`;
-        container.appendChild(grid);
-    }
+    clearGrid();
+    createGrid(sideQuantity);
 }
 
 let mouseHover = container.addEventListener('mouseover', paintBox)
 
 function paintBox(e) {
-    
     if (e.target.className === 'grid-element' && e.ctrlKey === false) {
         let selectedBox = e.target;
         selectedBox.style.backgroundColor = 'grey';
     }
-        //let gridElement = document.getElementsByClassName('grid-element');
-        //console.log(gridElement)
 }
 
 let footer = document.querySelector('footer');
 let resetButton = footer.querySelector('#reset-grid')
     resetButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log(e);
-
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    
-    createGrid(sideQuantity);
+        e.preventDefault();
+        clearGrid();
+        createGrid(sideQuantity);
     });
